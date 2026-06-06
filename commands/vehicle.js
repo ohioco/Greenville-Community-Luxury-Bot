29b16007bb086f869f5cae4582a4119920c70472
Vynren <150676621+ohioco@users.noreply.github.com>
2026-06-07 02:00:33 +1200

    Refactor vehicle command options to use EmbedBuilder

--- commands/vehicle.js
+++ commands/vehicle.js
@@ -1,55 +1,43 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const db = require("../db");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("vehicle")
     .setDescription("Register a vehicle")
-
-    .addStringOption(o =>
-      o
-        .setName("brand")
-        .setDescription("Vehicle brand (e.g Toyota)")
-        .setRequired(true)
-    )
-
-    .addStringOption(o =>
-      o
-        .setName("model")
-        .setDescription("Vehicle model (e.g Supra)")
-        .setRequired(true)
-    )
-
-    .addStringOption(o =>
-      o
-        .setName("color")
-        .setDescription("Vehicle color")
-        .setRequired(true)
-    )
-
-    .addStringOption(o =>
-      o
-        .setName("plate")
-        .setDescription("Vehicle plate number")
-        .setRequired(true)
-    ),
+    .addStringOption(o => o.setName("brand").setRequired(true))
+    .addStringOption(o => o.setName("model").setRequired(true))
+    .addStringOption(o => o.setName("color").setRequired(true))
+    .addStringOption(o => o.setName("plate").setRequired(true)),
 
   async execute(interaction) {
     const data = db.load();
+    if (!data.vehicles) data.vehicles = {};
 
     const id = interaction.user.id;
 
     if (!data.vehicles[id]) data.vehicles[id] = [];
 
-    data.vehicles[id].push({
+    const vehicle = {
       brand: interaction.options.getString("brand"),
       model: interaction.options.getString("model"),
       color: interaction.options.getString("color"),
       plate: interaction.options.getString("plate")
-    });
+    };
 
+    data.vehicles[id].push(vehicle);
     db.save(data);
 
-    await interaction.reply("🚗 Vehicle registered successfully!");
+    const embed = new EmbedBuilder()
+      .setTitle("Vehicle Registered")
+      .setColor(0x89CFF0)
+      .addFields(
+        { name: "Brand", value: vehicle.brand, inline: true },
+        { name: "Model", value: vehicle.model, inline: true },
+        { name: "Color", value: vehicle.color, inline: true },
+        { name: "Plate", value: vehicle.plate, inline: true }
+      );
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
   }
 };
