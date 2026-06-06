4b0c8a101607acd6b1f0c55cac93cc71d34609eb
Vynren <150676621+ohioco@users.noreply.github.com>
2026-06-06 23:42:10 +1200

    Add db.js for file read/write operations

---
+++ systems/db.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+function read(file) {
+  if (!fs.existsSync(file)) return {};
+  return JSON.parse(fs.readFileSync(file));
+}
+
+function write(file, data) {
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+module.exports = { read, write };
