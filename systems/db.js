6c551bdc357e8f5760724163a8174d7990914fbe
Vynren <150676621+ohioco@users.noreply.github.com>
2026-06-06 23:54:28 +1200

    Refactor db.js functions and add new methods

--- systems/db.js
+++ systems/db.js
@@ -1,12 +1,30 @@
 const fs = require("fs");
 
-function read(file) {
+function load(file) {
   if (!fs.existsSync(file)) return {};
   return JSON.parse(fs.readFileSync(file));
 }
 
-function write(file, data) {
+function save(file, data) {
   fs.writeFileSync(file, JSON.stringify(data, null, 2));
 }
 
-module.exports = { read, write };
+function get(file, key) {
+  const data = load(file);
+  return data[key];
+}
+
+function set(file, key, value) {
+  const data = load(file);
+  data[key] = value;
+  save(file, data);
+}
+
+function update(file, key, updater) {
+  const data = load(file);
+  if (!data[key]) data[key] = {};
+  data[key] = updater(data[key]);
+  save(file, data);
+}
+
+module.exports = { load, save, get, set, update };
