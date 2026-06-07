1737428243088d425b25482d75f78596f8dc26fd
Vynren <150676621+ohioco@users.noreply.github.com>
2026-06-07 22:28:31 +1200

    Add 'say' command for staff to send messages

---
+++ commands/say.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+const STAFF_ROLE = "1510346654241394848";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("say")
+    .setDescription("Send a message as the bot (Staff only)")
+    .addStringOption(o =>
+      o.setName("message")
+        .setDescription("The message to send")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    if (!interaction.member.roles.cache.has(STAFF_ROLE)) {
+      return interaction.reply({ content: "❌ You do not have permission to use this command.", ephemeral: true });
+    }
+
+    const message = interaction.options.getString("message");
+
+    await interaction.deferReply({ ephemeral: true });
+    await interaction.deleteReply();
+    await interaction.channel.send({ content: message });
+  }
+};
